1644f3796ce50a2b0c9d5380f9637a5c2d072abe
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-26 13:54:11 -0300

    adicionado styled-component ao projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import GlobalStyles from '../styles/GlobalStyles';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
