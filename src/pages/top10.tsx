346b57591cce2d36eb5d3d7309bc5480cb0da41d
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-26 16:16:39 -0300

    adicionada geraçao de paginas estaticas com recarregamento dinamico passando slug (parametros) na url

--- src/pages/top10.tsx
+++ src/pages/top10.tsx
@@ -28,13 +28,14 @@ export default function Top10({ products }: Top10Props) {
 }
 
 // Cria páginas estáticas, ótimo para conteúdos que sofrem poucas alterações
-export const getStatisProps: GetStaticProps<Top10Props> = async (context) => {
+export const getStaticProps: GetStaticProps<Top10Props> = async (context) => {
     const response = await fetch('http://localhost:3333/products');
     const products = await response.json();
 
     return {
         props: {
             products,
-        }
+        },
+        revalidate: 5,
     }
 }
