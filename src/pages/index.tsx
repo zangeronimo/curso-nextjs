9d78fdae50788b6ec1ab2d54b44c51c6f030e2a1
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-26 14:26:27 -0300

    fazendo chamada api com GetServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
+
+import { GetServerSideProps } from 'next';
 import { Title } from '../styles/pages/Home';
 
-export default function Home() {
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps) {
+
+  // Carregamento no client (não indexa nos motores de busca)
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/recommended').then(response => {
+  //     response.json().then(data => {
+  //       setRecommendedProducts(data);
+  //     });
+  //   });
+  // }, []);
+
   return (
     <div>
       <Title>Hello World</Title>
+
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
   )
+}
+
+// User somente para dados que precisam estar disponíveis ao motores de buscas
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/recommended');
+  const recommendedProducts = await response.json();
+
+  return {
+    props: {
+      recommendedProducts,
+    }
+  }
 }
